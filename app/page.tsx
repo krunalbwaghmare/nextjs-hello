f616b241e7bd0b33cbf6f9da95163af0e06897fd
krunalbwaghmare <142886177+krunalbwaghmare@users.noreply.github.com>
2024-04-12 15:20:28 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,3 @@
 export default function Page() {
-  return <h1>Hello, Next.js from local to github succesfully yooo yes </h1>;
+  return <h1>Hello, Next.js from local to github succesfully yooo b   yes </h1>;
 }
